dc38bd24e015ea24c8ce7b2a9055fb16e0183cc7
Nivedh Mudaliar <nivedh.mudaliar@gmail.com>
2013-11-16 14:56:28 -0800

    developed overall flow of application
    developed flow of application

---
+++ Poseidon/Poseidon/Poseidon/SignInViewController.m
@@ -0,0 +1,38 @@
+//
+//  SignInViewController.m
+//  Poseidon
+//
+//  Created by Nivedh Mudaliar on 11/16/13.
+//  Copyright (c) 2013 Shervin Shaikh. All rights reserved.
+//
+
+#import "SignInViewController.h"
+
+@interface SignInViewController ()
+
+@end
+
+@implementation SignInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Poseidon/Poseidon/Poseidon/SignUpViewController.m
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.m
+//  Poseidon
+//
+//  Created by Nivedh Mudaliar on 11/16/13.
+//  Copyright (c) 2013 Shervin Shaikh. All rights reserved.
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+
+@end
+
+@implementation SignUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
