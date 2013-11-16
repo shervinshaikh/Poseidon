d6cfd13e26e8a23f3ebbbad298fb757835fb72c9
Shervin Shaikh <shervin.shaikh@appfolio.com>
2013-11-16 10:29:41 -0800

    Custom callouts on pins

---
+++ Poseidon/SpotView.m
@@ -0,0 +1,31 @@
+//
+//  SpotView.m
+//  Poseidon
+//
+//  Created by Shervin Shaikh on 11/16/13.
+//  Copyright (c) 2013 Shervin Shaikh. All rights reserved.
+//
+
+#import "SpotView.h"
+
+@implementation SpotView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
