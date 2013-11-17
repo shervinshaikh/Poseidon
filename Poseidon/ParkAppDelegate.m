bdc036a6e45839bca6a412aeb65a86e9870a61ea
Nivedh Mudaliar <nivedh.mudaliar@gmail.com>
2013-11-17 08:57:04 -0800

    add view for checking what user wants to do
    initial view for checking whether user wants to rent a spot or is
    offering a spot to rent

--- Poseidon/ParkAppDelegate.m
+++ Poseidon/ParkAppDelegate.m
@@ -5,7 +5,7 @@
 //  Created by Shervin Shaikh on 11/16/13.
 //  Copyright (c) 2013 Shervin Shaikh. All rights reserved.
 //
-
+//this is a test
 #import "ParkAppDelegate.h"
 
 @implementation ParkAppDelegate
